b46c7f7f40aca17019d5b3645b6d5aa85a9edaea
Oliver Nightingale <oliver.nightingale1@gmail.com>
2013-10-05 14:19:49 +0100

    Add PiWire#init.

--- ext/pi_wire/pi_wire.c
+++ ext/pi_wire/pi_wire.c
@@ -1,10 +1,17 @@
 #include <ruby.h>
+#include <wiringPi.h>
 
 static VALUE hello_world(VALUE klass) {
   return rb_str_new2("Hello, World!");
 }
 
+static VALUE init_wiring_pi(VALUE klass) {
+  wiringPiSetup();
+  return Qtrue;
+}
+
 void Init_pi_wire() {
   VALUE mPiWire = rb_define_module("PiWire");
   rb_define_module_function(mPiWire, "hello_world", hello_world, 0);
+  rb_define_module_function(mPiWire, "init", init_wiring_pi, 0);
 }
--- spec/pi_wire_spec.rb
+++ spec/pi_wire_spec.rb
@@ -2,4 +2,8 @@ require 'spec_helper'
 
 describe PiWire do
   its(:hello_world) { should == "Hello, World!" }
+
+  describe "#init" do
+    it { PiWire.init.should be_true }
+  end
 end
