f57f568bf734804ac5071d555c095601106e2e5f
Oliver Nightingale <oliver.nightingale1@gmail.com>
2013-10-05 14:18:52 +0100

    Look for wiringPi lib before install.

--- ext/pi_wire/extconf.rb
+++ ext/pi_wire/extconf.rb
@@ -1,2 +1,30 @@
 require 'mkmf'
+
+LIBDIR = RbConfig::CONFIG['libdir']
+INCLUDEDIR = RbConfig::CONFIG['includedir']
+
+HEADER_DIRS = [
+  '/opt/local/include',
+  '/usr/local/include',
+  INCLUDEDIR,
+  '/usr/include'
+]
+
+LIB_DIRS = [
+  '/opt/local/lib',
+  '/usr/local/lib',
+  LIBDIR,
+  '/usr/lib'
+]
+
+dir_config('wiringPi', HEADER_DIRS, LIB_DIRS)
+
+unless find_header('wiringPi.h', *HEADER_DIRS)
+  abort 'wiringPi is missing, please install wiringPi'
+end
+
+unless find_library('wiringPi', nil, *LIB_DIRS)
+  abort 'wiringPi is missing, please install wiringPi'
+end
+
 create_makefile('pi_wire/pi_wire')
